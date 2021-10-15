1e38703b4d4b3cc166e2f2408dce75c9dba45b58
Alberto Alonso <alberto.entreprises@gmail.com>
2021-10-15 16:33:31 -0500

    working on Login vue, added template to the form

--- src/components/Login.Form.vue
+++ src/components/Login.Form.vue
@@ -1,3 +1,51 @@
 <template>
-    <div>Hello</div>
+  <v-card class="px-7">
+    <v-card-title class="mx-auto"><v-spacer/>
+      <h1 class="text-h4">LOGIN</h1><v-spacer/>
+    </v-card-title>
+    <v-form ref="form">
+      <v-text-field
+        v-model="model"
+        :counter="max"
+        :rules="rules"
+        label="First name"
+        class="mb-4 px-4"
+      ></v-text-field>
+
+      <v-text-field
+        v-model="email"
+        :rules="emailRules"
+        label="E-mail"
+        class="mb-4 px-4"
+        required
+      ></v-text-field>
+
+      <v-card-actions>
+        <v-btn 
+          block
+          color="primary"
+          depressed
+        >Ingresar</v-btn>
+      </v-card-actions>
+    </v-form>
+  </v-card>
 </template>
+
+<script>
+export default {
+  data: () => ({
+    valid: false,
+    firstname: "",
+    lastname: "",
+    nameRules: [
+      (v) => !!v || "Name is required",
+      (v) => v.length <= 10 || "Name must be less than 10 characters",
+    ],
+    email: "",
+    emailRules: [
+      (v) => !!v || "E-mail is required",
+      (v) => /.+@.+/.test(v) || "E-mail must be valid",
+    ],
+  }),
+};
+</script>
